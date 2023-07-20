bbe9074bd95838ff2ed57c0fdc9dda1e28ac452b
Suraj Purohit <86192644+SurajPurohit1110@users.noreply.github.com>
2023-07-20 18:26:29 +0530

    Update index.js by adding comments

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
-// const io = require('socket.io')(8000);
 const io = require('socket.io')(8000, {
     cors: {
         origin: "*",
@@ -13,15 +12,19 @@ const io = require('socket.io')(8000, {
 const users = {};
 
 io.on('connection', socket => {
+    // If any new user joins, let other users connected to the server know!
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
         io.emit('user-list', users);
     });
+    
+    // If someone sends a message, broadcast it to other people
     socket.on('send', message => {
         socket.broadcast.emit('recieve', { message: message, name: users[socket.id] });
     });
-
+    
+    // If someone leaves the chat, let others know
     socket.on('disconnect', message => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
